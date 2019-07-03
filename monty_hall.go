6a8eb211f644eedb0a7ad82bb737c23e5d0a422b
BH Kim <blue021@gmail.com>
2019-07-04 01:44:08 +0900

    어투 수정, 코드 린트 수정

--- monty_hall.go
+++ monty_hall.go
@@ -6,65 +6,65 @@ import (
 	"time"
 )
 
-func monty_hall(change_mind bool) bool {
+func montyHall(changeMind bool) bool {
 	rand.Seed(time.Now().UnixNano())
 
 	doors := [3]byte{'G', 'G', 'G'} // Goat
 	doors[rand.Intn(3)] = 'C'       // Car
 
-	first_selection := rand.Intn(3)
+	firstSelection := rand.Intn(3)
 
-	open_door := -1
-	if doors[first_selection] == 'C' {
+	openDoor := -1
+	if doors[firstSelection] == 'C' {
 		r := rand.Intn(2)
 
 		for i := 0; i < 3; i++ {
-			if first_selection == i {
+			if firstSelection == i {
 				continue
 			} else {
 				if r == 0 {
-					open_door = i
+					openDoor = i
 					break
 				}
-				r -= 1
+				r--
 			}
 		}
 
 	} else {
 		for i := 0; i < 3; i++ {
-			if i != first_selection && doors[i] == 'G' {
-				open_door = i
+			if i != firstSelection && doors[i] == 'G' {
+				openDoor = i
 				break
 			}
 		}
 	}
 
-	if true == change_mind {
-		second_selection := -1
+	if true == changeMind {
+		secondSelection := -1
 		for i := 0; i < 3; i++ {
-			if i != first_selection && i != open_door {
-				second_selection = i
+			if i != firstSelection && i != openDoor {
+				secondSelection = i
 				break
 			}
 
 		}
-		return doors[second_selection] == 'C'
+		return doors[secondSelection] == 'C'
 
 	} else {
-		return doors[first_selection] == 'C'
+		return doors[firstSelection] == 'C'
 	}
 }
 
-func simulate(n int, change_mind bool) {
+func simulate(n int, changeMind bool) {
 	count := 0
 
 	for i := 0; i < n; i++ {
-		if true == monty_hall(change_mind) {
-			count += 1
+		if true == montyHall(changeMind) {
+			count++
 		}
 	}
 
-	fmt.Printf("Change mind %5t => %d / %d (%0.3f %%)\n", change_mind, count, n, float64(count*100)/float64(n))
+	fmt.Printf("Change mind %5t => %d / %d (%0.3f %%)\n", changeMind, count, n, float64(count*100)/float64(n))
 }
 
 func main() {
